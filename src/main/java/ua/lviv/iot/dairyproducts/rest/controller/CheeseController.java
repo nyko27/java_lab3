6ff775191e293c03eaddd425863656675e8fa397
Yuriy Nykolyshak <yuranykolysak@gmail.com>
2020-04-28 05:13:48 +0300

    fixed controller and application.properties

--- src/main/java/ua/lviv/iot/dairyproducts/rest/controller/CheeseController.java
+++ src/main/java/ua/lviv/iot/dairyproducts/rest/controller/CheeseController.java
@@ -1,11 +1,7 @@
 package ua.lviv.iot.dairyproducts.rest.controller;
 
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +13,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import ua.lviv.iot.dairyproducts.business.CheeseService;
 import ua.lviv.iot.dairyproducts.model.Cheese;
 
@@ -25,7 +20,6 @@ import ua.lviv.iot.dairyproducts.model.Cheese;
 @RestController
 public class CheeseController {
 
-  private Map<Integer, Cheese> cheeses = new HashMap<>();
   private AtomicInteger idCounter = new AtomicInteger();
 
   @Autowired
@@ -33,42 +27,43 @@ public class CheeseController {
 
   @GetMapping
   public List<Cheese> getCheeses() {
-    cheeseService.findAllCheeses();
-    return new LinkedList<Cheese>(cheeses.values());
+    return cheeseService.findAllCheeses();
   }
 
   @GetMapping(path = "/{id}")
-  public Cheese getCheese(final @PathVariable("id") Integer cheeseId) {
-    cheeseService.findCheese(cheeseId);
-    return cheeses.get(cheeseId);
+  public ResponseEntity<Cheese> getCheese(final @PathVariable("id") Integer cheeseId) {
+    Cheese thisCheese;
+    thisCheese = cheeseService.findCheese(cheeseId);
+    if (thisCheese != null) {
+      return new ResponseEntity<Cheese>(thisCheese, HttpStatus.OK);
+    }
+    return new ResponseEntity<Cheese>(HttpStatus.NOT_FOUND);
   }
 
   @PostMapping
   public Cheese createCheese(final @RequestBody Cheese cheese) {
     cheeseService.createCheese(cheese);
     cheese.setId(idCounter.incrementAndGet());
-    cheeses.put(cheese.getId(), cheese);
     return cheese;
   }
 
   @DeleteMapping(path = "/{id}")
   public ResponseEntity<Cheese> deleteCheese(@PathVariable("id") Integer cheeseId) {
-    HttpStatus status = cheeses.remove(cheeseId) == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
-    if (getCheese(cheeseId) != null) {
+    Cheese thisCheese;
+    thisCheese = cheeseService.findCheese(cheeseId);
+    if (thisCheese != null) {
       cheeseService.deleteCheese(cheeseId);
-      return new ResponseEntity<Cheese>(status);
+      return new ResponseEntity<Cheese>(HttpStatus.OK);
     }
-    return ResponseEntity.status(status).build();
+    return new ResponseEntity<Cheese>(HttpStatus.NOT_FOUND);
   }
 
   @PutMapping(path = "/{id}")
   public ResponseEntity<Cheese> updateCheese(final @PathVariable("id") Integer cheeseId,
       final @RequestBody Cheese newCheese) {
-    Cheese oldcheese = cheeses.get(cheeseId);
-
+    Cheese oldcheese = cheeseService.findCheese(cheeseId);
     if (oldcheese != null) {
       cheeseService.updateCheese(cheeseId, newCheese);
-      cheeses.put(cheeseId, newCheese);
       return new ResponseEntity<Cheese>(oldcheese, HttpStatus.OK);
     }
     return new ResponseEntity<Cheese>(HttpStatus.NOT_FOUND);
